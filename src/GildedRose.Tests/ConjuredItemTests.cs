fb1aebdd2fdaf02a25bfec72bdeb13afbbade99f
Chris Auckland <chris.auckland@red-gate.com>
2016-02-29 16:03:27 +0000

    Red: a conjured item should have its quality degrade by two each iteration

---
+++ src/GildedRose.Tests/ConjuredItemTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    public class ConjuredItemTests
+    {
+        [Test]
+        public void ConjuredItemsDegradeByTwoEachIteration()
+        {
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "Conjured Item", SellIn = 10, Quality = 10}
+                }
+
+            };
+
+            app.UpdateQuality();
+
+            Assert.AreEqual(8, app.Items[0].Quality);
+        }
+    }
+}
